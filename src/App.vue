afca32fb0902b3a4fe353ab14ce0947c13727f4d
LydiahKamuyu <kamuyulydia@gmail.com>
2023-09-22 16:07:19 +0300

    added code to the Modal component, also imporeted, registered and used the Modal / template in the App Vue component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
   <h1>{{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick"> ClickMe </button>
+  <Modal/>
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
+  components: {Modal},
+  
   data() {
     return {
       title: 'Hello Vue.js!'
-    }
-  },
-  methods: {
-    handleClick() {
-      console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
     }
   }
 
@@ -31,10 +27,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-h1 {
-  border-bottom: 1px solid #ddd;
-  display: inline-block;
-  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="backdrop">
+        <div class="model">
+            <p>modal content</p>
+        </div>
+    </div>
+</template>
+
+<style>
+.backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  background: white;
+  width: 400px;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  text-align: center;
+}
+
+.modal p {
+  font-size: 18px;
+  margin-bottom: 20px;
+}
+
+/* Add any additional styles or customizations here */
+</style>
